0cca75c0d3880ae8826eb318cc99ac1afe3ea0c8
rakki-18 <rakki@gmail.com>
2020-10-06 16:52:56 +0530

    Added a problem

---
+++ 1422A.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+
+typedef   long long  int ll;
+typedef  long double ld;
+typedef std::vector<ll> vi;
+typedef std::vector<std::vector<ll> > vv;
+
+#define MOD 1000000007
+#define IO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fo(i,a,b) for(i=a;i<b;i++)
+#define mp make_pair
+#define pb(x) push_back(x)
+#define fi first
+#define se second
+#define print(vec,a,b) for(ll i=a;i<b;i++) cout<<vec[i]<<" ";cout<<endl;
+#define all(a) a.begin(),a.end()
+#define input(vec,a,b) for(ll i = a;i<b;i++) cin>>vec[i];
+#define ms(a,val) memset(a,val,sizeof(a))
+using namespace std;
+
+const int N = 1e5 + 5;
+
+
+void solve(){
+            ll i,j,n,m,k;
+           
+          ll a,b,c;
+          cin>>a>>b>>c;
+          cout<<max(a,max(b,c)) + min(a,min(b,c))<<endl;
+			
+            
+				    
+            
+        
+    
+            
+						            
+
+
+
+
+
+
+
+
+
+}
+
+int main()
+{
+    IO;
+      ll t=1;
+      cin>>t;
+
+
+      while(t--)
+      {
+
+          solve();
+      }
+    return 0;
+}
+
